1fc08d745466f9986840d561c9f4ab2df7ad63e5
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-23 09:41:45 -0500

    Add files via upload

---
+++ PrimerosProtectos/Tree.java
@@ -0,0 +1,20 @@
+
+package tree;
+
+class Tree  
+{    
+    public static void main(String args[]) {  
+       System.out.println("Hello, my name is Natalia and i'm learning java :))");
+       System.out.println("I'm going to plant a tree today" + " Ready to get my hands dirty!");
+       System.out.println("  ****   ");
+       System.out.println(" ******  ");
+       System.out.println("********");
+       System.out.println("********");
+       System.out.println(" ******  ");
+       System.out.println("  ****  ");
+       System.out.println("   **  ");
+       System.out.println("   **  ");
+       System.out.println("   **  ");
+       System.out.println("   **  ");
+   }  
+}  
