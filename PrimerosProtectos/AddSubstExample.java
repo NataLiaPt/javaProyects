d80a5a332641d88027d89ae2ea4092c4ecb3fbd1
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-23 09:50:39 -0500

    Add files via upload

---
+++ PrimerosProtectos/AddSubstExample.java
@@ -0,0 +1,16 @@
+package addsubstexample;
+
+public class AddSubstExample {
+
+    public static void main(String[] args) {
+        int bearsInZoo = 8;
+    int penguinsInZoo = 4;
+    int animalsInZoo = bearsInZoo + penguinsInZoo;
+    
+    System.out.println(animalsInZoo); 
+
+    int bearsAfterTrade = bearsInZoo - 2;
+
+    System.out.println(bearsAfterTrade);
+	}
+    }
