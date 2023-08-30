0c82dea760814c8e8e85dc83eaacbddfa4e2e246
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 09:49:14 -0500

    Add files via upload

---
+++ PrimerosProtectos/StringP.java
@@ -0,0 +1,10 @@
+package stringp;
+
+public class StringP {
+
+    public static void main(String[] args) {
+        String txt = "Today my day seems really sad";
+        System.out.println(txt);
+    }
+    
+}
