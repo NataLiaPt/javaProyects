b197919f590c1a9f7dfd4dd857b4e3d8a8cccca2
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 08:31:51 -0500

    Add files via upload

---
+++ PrimerosProtectos/Concatenation.java
@@ -0,0 +1,14 @@
+package concatenation;
+
+public class Concatenation {
+
+    public static void main(String[] args) {
+        int cars = 12;
+        String brand = "ferrari";
+        String description = "Our concessionaire has " + cars + " " + brand + "s."; 
+        System.out.println(description);
+
+        
+    }
+    
+}
