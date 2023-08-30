7140a8a7c65060ed05ec2b5b87ea92c9e0c1a448
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 09:52:04 -0500

    Add files via upload

---
+++ PrimerosProtectos/Variable.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        String name = "Cristian D Ronaldo";
+        int year = 1975;
+        System.out.println(name);
+        System.out.println(year);
+    }
+    
+}
