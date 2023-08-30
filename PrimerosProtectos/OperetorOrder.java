09f46bff747be291dc2a02d38842aafaf08d6d37
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 09:24:03 -0500

    Add files via upload

---
+++ PrimerosProtectos/OperetorOrder.java
@@ -0,0 +1,16 @@
+package operetororder;
+
+public class OperetorOrder {
+
+    public static void main(String[] args) {
+        int operation1 = 5 % 20 - (4 * 5 - 3);
+        System.out.println(operation1);
+        
+        int operation2 = (4 + (2 * 7 - 5)) + 7 - 3;
+        System.out.println(operation2);
+        
+        int operation3 = 5 * 4 % 3 - 4 + 2;
+        System.out.println(operation3);
+    }
+    
+}
