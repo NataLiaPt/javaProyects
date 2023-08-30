89e71ede4642255571bc780e284e0d0488feae94
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 09:10:18 -0500

    Add files via upload

---
+++ PrimerosProtectos/Module.java
@@ -0,0 +1,11 @@
+package module;
+
+public class Module {
+
+    public static void main(String[] args) {
+        int people = 34;
+        int left = people%3;
+        System.out.println(left);
+    }
+    
+}
