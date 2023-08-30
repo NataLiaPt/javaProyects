86d235c45fc2c79045feae96cb7a226e7e2c833e
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 09:01:22 -0500

    Add files via upload

---
+++ PrimerosProtectos/Keywords.java
@@ -0,0 +1,12 @@
+package keywords;
+
+public class Keywords {
+
+    public static void main(String[] args) {
+        final double twoSqr = 1.41421;
+        System.out.println(twoSqr);
+        twoSqr *= 2; // It cannot change the value
+       
+    }
+    
+}
