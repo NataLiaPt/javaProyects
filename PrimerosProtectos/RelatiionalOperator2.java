00eb76078dca25217a894acec841d2d1f94f0f14
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-30 09:39:00 -0500

    Add files via upload

---
+++ PrimerosProtectos/RelatiionalOperator2.java
@@ -0,0 +1,14 @@
+package relatiionaloperator2;
+
+public class RelatiionalOperator2 {
+
+    public static void main(String[] args) {
+        int songA = 9;
+        int songB = 8;
+        
+        int albumLengthA = 32;
+        int albumLengthB = 54;
+        boolean sameNumerOfSongs = songA == songB;
+        boolean differentLength = albumLengthA != albumLengthB;
+    }
+}
