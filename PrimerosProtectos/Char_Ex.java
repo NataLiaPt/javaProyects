f17dcae757d1c456e642c46f86f092f37400d7db
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-08-29 21:08:20 -0500

    Add files via upload

---
+++ PrimerosProtectos/Char_Ex.java
@@ -0,0 +1,9 @@
+package char_ex;
+
+public class Char_Ex {
+    public static void main(String[] args) {
+        char gradePrediction = 'C';
+        System.out.println(gradePrediction);
+    }
+    
+}
