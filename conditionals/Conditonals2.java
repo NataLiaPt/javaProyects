9c91e23425a40962c628d00098f821c75a6a5982
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-09-06 09:48:44 -0500

    Add files via upload

---
+++ conditionals/Conditonals2.java
@@ -0,0 +1,16 @@
+package conditonals2;
+
+public class Conditonals2 {
+
+    public static void main(String[] args) {
+        int x = 56;
+        int y = 230;
+        if (x+y < 100) {
+            System.out.println("x + y is less than 100");
+        }
+        else {
+            System.out.println("x + y is bigger than 100");
+        }
+    }
+    
+}
