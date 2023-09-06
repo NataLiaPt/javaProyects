054e4abb4b9892451008692f98dfa2d1ffa41d39
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-09-06 09:45:41 -0500

    Add files via upload

---
+++ conditionals/Conditionals1.java
@@ -0,0 +1,13 @@
+package conditionals1;
+
+public class Conditionals1 {
+
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 72;
+        if(x+y > 70) {
+            System.out.println("x + y is bigger than 70");
+        }
+    }
+    
+}
