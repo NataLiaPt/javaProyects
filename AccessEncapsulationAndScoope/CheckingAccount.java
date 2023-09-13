9ff952521025417a018361c79a56ccbea259b59a
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-09-13 08:43:49 -0500

    Add files via upload

---
+++ AccessEncapsulationAndScoope/CheckingAccount.java
@@ -0,0 +1,11 @@
+package bank;
+
+public class CheckingAccount{
+  public String name;
+  public int balance;
+
+  public CheckingAccount(String inputName, int inputBalance){
+    name = inputName;
+    balance = inputBalance;
+  }
+}
