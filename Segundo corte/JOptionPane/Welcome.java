62dd0d98753f6d6a9f16b7d12ec2546b33dd41ef
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-10-05 09:06:02 -0500

    Add files via upload

---
+++ Segundo corte/JOptionPane/Welcome.java
@@ -0,0 +1,10 @@
+package Welcome;
+
+import javax.swing.JOptionPane;
+
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Welcome\n to\n the\n java world");
+        System.exit(0);
+    }
+}
