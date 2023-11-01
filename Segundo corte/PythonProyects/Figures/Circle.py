c70a52370da8db3ab80f00ab300121fad1a50828
NataLiaPt <143001700+NataLiaPt@users.noreply.github.com>
2023-10-31 22:13:49 -0500

    Update Circle.py

--- Segundo corte/PythonProyects/Figures/Circle.py
+++ Segundo corte/PythonProyects/Figures/Circle.py
@@ -3,7 +3,8 @@ class Circle:
     
     def __init__(self, diameter):
         self.radius = diameter / 2
-    def area (self):
-        return self.pi*self.radius**2
     def perimeter (self):
         return self.pi*2*self.radius
+    def area (self):
+        return self.pi*self.radius**2
+    
